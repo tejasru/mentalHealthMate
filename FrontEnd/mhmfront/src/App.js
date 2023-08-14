a1510594689281aa1fc41d0820083bee38d80689
hisoka7i <rohit7i@outlook.com>
2023-08-14 19:32:07 +0530

    login page added

--- FrontEnd/mhmfront/src/App.js
+++ FrontEnd/mhmfront/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import './App.css';
 import Navbar from './components/NavBar';
+import Login from './pages/Login'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => {
   return (
+    <>
     <Navbar></Navbar>
+    <Login></Login>
+    </>
   );
 };
 
---
+++ FrontEnd/mhmfront/src/pages/Login.js
@@ -0,0 +1,44 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import './styles/Login.css';
+import {AiOutlineUser} from 'react-icons/ai';
+
+// import loginpic from "../images/login.svg"
+
+const Login = () =>{
+    return (
+        <>
+        <div class="wrapper">
+    <div class="container main">
+        <div class="row">
+            <div class="col-md-6 side-image">        
+            </div>
+            <div class="col-md-6 right">
+                
+                <div class="input-box">
+                   
+                   <header><h2>Login <AiOutlineUser /></h2></header>
+                   <div class="input-field">
+                        <input type="text" class="input" id="email" required="" autocomplete="off" />
+                        <label for="email">Email</label> 
+                    </div> 
+                   <div class="input-field">
+                        <input type="password" class="input" id="pass" required="" />
+                        <label for="pass">Password</label>
+                    </div> 
+                   <div class="input-field">
+                        
+                        <input type="submit" class="login" value="Login" />
+                   </div> 
+                   <div class="signin">
+                    <span>For Registeration: <a href="#">Register here</a></span>
+                   </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+</div>
+        </>
+    )
+}
+export default Login;
