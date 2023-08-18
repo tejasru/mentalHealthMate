a10015ff8dda2653f9b3df07f1d405439ddb600f
tejasru <tejaruikar@gmail.com>
2023-08-18 15:19:00 +0530

    Done

---
+++ Database/mhm_system_doctors.sql
@@ -0,0 +1,7 @@
+INSERT INTO doctors (username, password, mobile, qualification, name)
+VALUES
+    ('dr_amit', 'amit123', '9876543210', 'MBBS, MD (Internal Medicine)', 'Dr. Amit Sharma'),
+    ('dr_neha', 'neha123', '9876543211', 'MBBS, MS (Obstetrics and Gynaecology)', 'Dr. Neha Verma'),
+    ('dr_raj', 'raj123', '9876543212', 'MBBS, MS (Orthopedics)', 'Dr. Raj Singh'),
+    ('dr_anaya', 'anaya123', '9876543213', 'MBBS, DNB (Pediatrics)', 'Dr. Anaya Patel'),
+    ('dr_vikas', 'vikas123', '9876543214', 'MBBS, DM (Cardiology)', 'Dr. Vikas Yadav');
---
+++ Database/mhm_system_users.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO users (email, city, street, pincode, username, gender, age, mobile, password)
+VALUES
+    ('amit.sharma@gmail.com', 'Mumbai', '123 Gandhi Rd', '400001', 'amitsharma', 'Male', 28, '9876543210', 'amit123'),
+    ('neha.verma@yahoo.com', 'Delhi', '456 Nehru Nagar', '110001', 'nehav', 'Female', 25, '9876543211', 'neha123'),
+    ('raj.singh@gmail.com', 'Bangalore', '789 MG Road', '560001', 'rajsingh', 'Male', 32, '9876543212', 'raj123'),
+    ('anaya.patel@gmail.com', 'Ahmedabad', '101 Patel Chowk', '380001', 'anayapatel', 'Female', 23, '9876543213', 'anaya123'),
+    ('vikas.yadav@live.com', 'Jaipur', '202 JLN Marg', '302001', 'vikasyadav', 'Male', 29, '9876543214', 'vikas123');
+	
+
